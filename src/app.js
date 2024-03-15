137d40f202585d7415e82a72810b3d4ed4e38904
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-03-15 03:41:28 +0000

    [se realizan los tres ejercicios propuestos] primero el de solo generar dominios, luego generarlos con dominios aleatorios y por ultimo generarlo cuando cuando el nombre incluye el dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,108 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+/*
+ejercicio numero 1
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+thegreatjogger.com
+thegreatracoon.com
+ourgreatjogger.com
+ourgreatracoon.com
+thebigjogger.com
+thebigracoon.com
+ourbigjogger.com
+ourbigracoon.com
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+
+for(let i=0;i<pronoun.length;i++){
+
+  for(let j=0;j<adj.length;j++){
+
+    for(let k=0;k<noun.length;k++){
+      console.log(pronoun[i] + adj[j] + noun[k] +".com");
+    }
+  }
+
+}
+
+otra forma de entregar el mismo ejercicio numero 1
+  
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for(let p of pronoun){
+  for(let a of adj){
+    for(let n of noun){
+      console.log(p+a+n+".com");  
+    }
+  }
+
+}
+
+
+*/
+
+
+/* ejercicio dos cambio de extension
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racom'];
+let extensiones=['.com', '.net', '.org', '.io', '.gov', '.edu', '.info', '.biz', '.co', '.tv','.es'];
+
+
+
+
+for(let p of pronoun){
+  for(let a of adj){
+    for(let n of noun){
+      console.log(p+a+n+extensiones[Math.floor(Math.random()*extensiones.length)]);  
+    }
+  }
+
+}
+
+*/ 
+
+//ejercicio Agrega "domain hacks" donde la extensión forma parte del dominio
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['aico','jogger', 'racom', 'puedes'];
+let extensiones=['com', 'net', 'org', 'io', 'gov', 'edu', 'info', 'biz', 'co', 'tv','es'];
+
+let dominio="";
+
+
+
+for(let p of pronoun){
+  for(let a of adj){
+    for(let n of noun){
+      dominio=(p+a+n);  
+
+      let resultado='';
+
+      for(let indice of extensiones){
+        let num=indice.length;
+        let rebanada= dominio.slice(dominio.length-num,dominio.length);
+        if(rebanada==indice){
+          let arreglo=dominio.split('');
+          arreglo.splice(arreglo.length-num,0,'.');
+          resultado=arreglo.join('');
+        }
+      }
+
+      if(resultado==''){
+        resultado=p+a+n+'.'+extensiones[Math.floor(Math.random()*extensiones.length)]; 
+        console.log(resultado);
+      }else{
+        console.log(resultado);
+      }
+
+    }
+  }
+
+}
